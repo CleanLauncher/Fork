a6609bf52868057d7ff90882c54bd29a82fc91ec
Alessio Attilio <attilio.alessio@protonmail.com>
2026-07-28 12:29:09 +0200

    fix: revert website submodule, add sync workflow to push website to CleanLauncher/website
    Signed-off-by: Alessio Attilio <attilio.alessio@protonmail.com>

---
+++ website/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import HeroSection from './components/HeroSection.vue'
+import FeaturesSection from './components/FeaturesSection.vue'
+import ReleasesSection from './components/ReleasesSection.vue'
+import InstallSection from './components/InstallSection.vue'
+import FooterSection from './components/FooterSection.vue'
+</script>
+
+<template>
+  <NavBar />
+  <main>
+    <HeroSection />
+    <FeaturesSection />
+    <InstallSection />
+    <ReleasesSection />
+  </main>
+  <FooterSection />
+</template>
